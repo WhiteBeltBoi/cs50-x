d4a06e1e1ad4c103b2a1999576e89b238d981ecc
WhiteBeltBoi <gergelypifko@gmail.com>
2026-04-19 17:52:09 +0200

    Fixed the bug by not using strtok, and just looping through the characters, and initializing the word_counter with 1, instead of 0, because the last word doesn't end on a space.

--- week_02/readability/readability.c
+++ week_02/readability/readability.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
 int grader(string);
 int main(void){
@@ -24,22 +25,21 @@ int grader(string my_sentence){
     int letter_counter = 0;
     int sentence_counter = 0;
     int word_counter = 0;
-    string word= strtok(my_sentence, " ");
-    while (word != NULL) {
-    word_counter += 1;
-    int n = strlen(word);
-    letter_counter += n;
-    for (int i = 0; i < n; i++){
-        if (word[i] == '!' || word[i] == '.' || word[i] =='?'){
+    for (int i = 0, n = strlen(my_sentence); i < n; i++){
+        if (isalpha(my_sentence[i])){
+            letter_counter += 1;
+        }
+        else if (my_sentence[i] == ' '){
+            word_counter += 1;
+        }
+        else if (my_sentence[i] == '.'|| my_sentence[i] == '!' || my_sentence[i] == '?'){
             sentence_counter += 1;
-            letter_counter -=1;
         }
     }
-    word = strtok(NULL, " ");
-  }
+
   float L = ((float) letter_counter / word_counter) * 100;
   float S = ((float) sentence_counter / word_counter) * 100;
-  return (int) round(0.0588 * L - 0.296 * S - 15.8);
+  return  (int) round(0.0588 * L - 0.296 * S - 15.8);
 
 }
 
