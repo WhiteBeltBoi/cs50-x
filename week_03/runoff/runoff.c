02c2d9496f494c938382e2e11c2c1e1d3f35bbed
WhiteBeltBoi <gergelypifko@gmail.com>
2026-04-23 16:11:29 +0200

    Maybe finished

--- week_03/runoff/runoff.c
+++ week_03/runoff/runoff.c
@@ -174,20 +174,34 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min = candidates[0].votes;
+    for (int i = 0; i < candidate_count; i++){
+        if (!candidates[i].eliminated && candidates[i].votes < min){
+            min = candidates[i].votes;
+        }
+    }
+
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++){
+        if(candidates[i].votes > min){
+            return false;
+        }
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for(int i = 0; i < candidate_count; i++){
+        if(candidates[i].votes == min){
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
