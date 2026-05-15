8a8868b9e3d08a79bb57cf321d529b60d7e19461
WhiteBeltBoi <gergelypifko@gmail.com>
2026-05-15 12:14:47 +0200

    Fixed the bug in sepia. I rounder each modified color 1 by 1. I should just round the final value of each color channel after the addition of the factors.

--- week_04/filter-less/filter-less/helpers.c
+++ week_04/filter-less/filter-less/helpers.c
@@ -21,17 +21,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
-            int sepiaRed = round(0.393 * image[i][j].rgbtRed) + round(0.769 * image[i][j].rgbtGreen) + round(0.189 * image[i][j].rgbtBlue);
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue);
             if(sepiaRed > 255){
                 sepiaRed = 255;
             }
 
-            int sepiaGreen = round(0.349 * image[i][j].rgbtRed) + round(0.686 * image[i][j].rgbtGreen) + round(0.168 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed) +(0.686 * image[i][j].rgbtGreen) +(0.168 * image[i][j].rgbtBlue);
             if(sepiaGreen > 255){
                 sepiaGreen = 255;
             }
 
-            int sepiaBlue  = round(0.272 * image[i][j].rgbtRed) + round(0.534 * image[i][j].rgbtGreen) + round(0.131 * image[i][j].rgbtBlue);
+            int sepiaBlue  = round(0.272 * image[i][j].rgbtRed) +(0.534 * image[i][j].rgbtGreen) +(0.131 * image[i][j].rgbtBlue);
             if(sepiaBlue  > 255){
                 sepiaBlue  = 255;
             }
